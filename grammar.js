ab9478556c5d903a4a6e3d11d780be6561af584f
Andrew Helwer <ahelwer@microsoft.com>
2020-06-27 22:40:03 -0400

    Implemented expression

--- grammar.js
+++ grammar.js
@@ -1,6 +1,8 @@
 module.exports = grammar({
   name: 'PRISM',
 
+  /*extras: $ => [],*/
+
   rules: {
     source_file: $ =>
       seq(
@@ -27,7 +29,7 @@ module.exports = grammar({
       field('type', $.primitive_type),
       field('name', $.identifier),
       '=',
-      field('value', $.value),
+      field('value', $.expression),
       ';',
     ),
 
@@ -43,7 +45,7 @@ module.exports = grammar({
       field('name', $.identifier),
       ':',
       field('type', choice($.range, $.primitive_type)),
-      field('init', optional(seq('init', $.value))),
+      field('init', optional(seq('init', $.expression))),
       ';'
     ),
 
@@ -58,14 +60,8 @@ module.exports = grammar({
       ';',
     ),
 
-    // TODO
-    guard: $ => choice(
-      seq(
-        $.identifier,
-        $.binary_function,
-        $.value,
-      ),
-    ),
+    // TODO: ensure boolean
+    guard: $ => $.expression,
 
     probabilistic_updates: $ => seq(
       repeat(seq($.probabilistic_update, '+')),
@@ -73,7 +69,7 @@ module.exports = grammar({
     ),
 
     probabilistic_update: $ => seq(
-      $.probability,
+      choice($.probability, $.rate),
       ':',
       $.updates
     ),
@@ -92,44 +88,71 @@ module.exports = grammar({
       ')'
     ),
 
-    binary_function: $ => choice(
-      '=',
-      '!=',
-      '>',
-      '<',
-      '>=',
-      '<=',
+    expression: $ => choice(
+      $.value,
+      $.identifier,
+      $.infix_function,
+      $.built_in_function,
     ),
 
-    boolean_function: $ => choice(
-      '&',
-      '|',
+    infix_function: $ => choice(
+      prec.left(1, seq($.expression, '?', $.expression, ':', $.expression)),
+      prec.left(2, seq($.expression, '=>', $.expression)),
+      prec.left(3, seq($.expression, '<=>', $.expression)),
+      prec.left(3, seq($.expression, '<=>', $.expression)),
+      prec.left(4, seq($.expression, '|', $.expression)),
+      prec.left(5, seq($.expression, '&', $.expression)),
+      prec.left(6, seq('!', $.expression)),
+      prec.left(7, seq($.expression, '=', $.expression)),
+      prec.left(7, seq($.expression, '!=', $.expression)),
+      prec.left(8, seq($.expression, '<', $.expression)),
+      prec.left(8, seq($.expression, '<=', $.expression)),
+      prec.left(8, seq($.expression, '>=', $.expression)),
+      prec.left(8, seq($.expression, '>', $.expression)),
+      prec.left(9, seq($.expression, '+', $.expression)),
+      prec.left(9, seq($.expression, '-', $.expression)),
+      prec.left(10, seq($.expression, '*', $.expression)),
+      prec.left(10, seq($.expression, '/', $.expression)),
+      prec.left(11, seq('-', $.expression)),
+    ),
+
+    built_in_function: $ => choice(
+      seq('min(', repeat1(seq($.expression, ',')), $.expression, ')'),
+      seq('max(', repeat1(seq($.expression, ',')), $.expression, ')'),
+      seq('floor(', $.expression, ')'),
+      seq('ceil(', $.expression, ')'),
+      seq('round(', $.expression, ')'),
+      seq('pow(', $.expression, ',', $.expression, ')'),
+      seq('mod(', $.expression, ',', $.expression, ')'),
+      seq('log(', $.expression, ',', $.expression, ')'),
     ),
 
     primitive_type: $ => choice(
       'bool',
       'int',
+      'double'
     ),
 
     range: $ => seq(
       '[',
-      field('low', $.number),
+      field('low', $.expression),
       '..',
-      field('high', $.number),
+      field('high', $.expression),
       ']',
     ),
 
-    // TODO
-    expression: $ => $.value,
-
     identifier: $ => /[A-Za-z_][A-Za-z0-9_]*/,
 
-    value: $ => choice($.number, $.boolean),
+    value: $ => choice($.integer, $.boolean, $.double),
 
-    number: $ => /\d+/,
+    probability: $ => $.double,
+
+    rate: $ => $.integer,
+
+    integer: $ => /\d+/,
+
+    double: $ => /\d+(\/\d+|\.\d+)?/,
 
     boolean: $ => choice('true', 'false'),
-
-    probability: $ => '0.5',
   }
 });
